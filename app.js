d0345bb6211510baedfb8ced33f013fa977ce8a3
devinda9936 <devindanishshanka2003@gmail.com>
2024-06-26 15:58:16 +0530

    friest commit

---
+++ app.js
@@ -0,0 +1,16 @@
+function addCustomer(){
+
+    
+    let age = document.getElementById("gnumber").value;
+    
+
+    
+}
+let randomNumber = Math.floor((Math.random()*10)+1);
+console.log(randomNumber);
+if (randomNumber==gnumber){
+ console.log ('win you' );
+
+}else{
+    console.log(randomNumber);  
+}
